dc3002ebcb390c877f02a22cf82ce2282b8957fb
bianpeng <695429818@qq.com>
2020-04-29 16:26:02 +0800

    init

---
+++ src/main/java/com/gitee/occo/gateway/config/SentinelDataSourceNacos.java
@@ -0,0 +1,70 @@
+package com.gitee.occo.gateway.config;
+
+import java.util.List;
+import java.util.Properties;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.csp.sentinel.datasource.ReadableDataSource;
+import com.alibaba.csp.sentinel.datasource.nacos.NacosDataSource;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.nacos.api.PropertyKeyConst;
+
+/**
+ * sentinel数据持久化到nacos
+ * @see https://github.com/alibaba/Sentinel/wiki/%E5%8A%A8%E6%80%81%E8%A7%84%E5%88%99%E6%89%A9%E5%B1%95
+ * @author kinbug
+ */
+@Component
+public class SentinelDataSourceNacos {
+	
+	// nacos server addr
+	@Value("${spring.cloud.nacos.config.server-addr}")
+	private String serverAddr;
+
+	// nacos group
+	private static final String groupId = "DEFAULT_GROUP";
+	// nacos dataId
+	private static final String dataId = "gateway-sentinel";
+
+	// if change to true, should be config NACOS_NAMESPACE_ID
+	private static boolean isDemoNamespace = false;
+	// fill your namespace id,if you want to use namespace. for example:
+	// 0f5c7314-4983-4022-ad5a-347de1d1057d,you can get it on nacos's console
+	private static final String NACOS_NAMESPACE_ID = "${namespace}";
+
+	@PostConstruct
+	public void initRules() {
+		if (isDemoNamespace) {
+			loadMyNamespaceRules();
+		} else {
+			loadRules();
+		}
+	}
+
+	private void loadRules() {
+		System.err.println("==================================");
+		ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new NacosDataSource<>(serverAddr, groupId,
+				dataId, source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {
+				}));
+		FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+	}
+
+	private void loadMyNamespaceRules() {
+		Properties properties = new Properties();
+		properties.put(PropertyKeyConst.SERVER_ADDR, serverAddr);
+		properties.put(PropertyKeyConst.NAMESPACE, NACOS_NAMESPACE_ID);
+
+		ReadableDataSource<String, List<FlowRule>> flowRuleDataSource = new NacosDataSource<>(properties, groupId,
+				dataId, source -> JSON.parseObject(source, new TypeReference<List<FlowRule>>() {
+				}));
+		FlowRuleManager.register2Property(flowRuleDataSource.getProperty());
+	}
+
+}
